2e03f7d6c9260987ca34d33907a17437b2ad7bad
Helena Ferry <helena.ferry@gmail.com>
2022-10-21 15:17:45 +0200

    Somewhat better add-form

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,33 +1,25 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from "react";
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Head from 'next/head'
 import PageTemplate from "./../components/page-template/PageTemplate"
-import Plate from './../components/plate/Plate'
+import AddForm from './../components/add-form/AddForm'
 
 const Add: NextPage = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
 
+  let [plateNumber, setPlateNumber] = useState<string>('');
 
-  async function addSpotting() {
-    const { data, error } = await supabase
-      .from('spottings')
-      .insert(
-        { plateNumber: '001', note: 'tjo', email: session?.user.email }
-      )
-    console.log(data, error)
-  }
+
+
 
   return (
     <div>
-
-          <PageTemplate>
-            <button onClick={addSpotting} className="btn-primary">
-              Add
-            </button>
-          </PageTemplate>
-
+      <PageTemplate>
+        <AddForm />
+      </PageTemplate>
     </div>
   )
 }
