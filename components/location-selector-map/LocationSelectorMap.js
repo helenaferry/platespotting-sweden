de812ef6629052e6a62301e7eb55bd608ec73043
Helena Ferry <helena.ferry@gmail.com>
2022-11-08 20:46:21 +0100

    Add search control to map

--- components/location-selector-map/LocationSelectorMap.js
+++ components/location-selector-map/LocationSelectorMap.js
@@ -2,6 +2,8 @@
 import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
 import { useState, useRef, useMemo, useCallback, useEffect } from 'react'
 import 'leaflet/dist/leaflet.css'
+import 'node_modules/leaflet-geosearch/dist/geosearch.css'
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 
 export default function LocationSelectorMap(props) {
   const markerRef = useRef(null)
@@ -38,8 +40,9 @@ export default function LocationSelectorMap(props) {
         dragend() {
           const marker = markerRef.current
           if (marker != null) {
+            console.log('dragend', marker.getLatLng())
             setPosition(marker.getLatLng())
-            props.updateLocation(position)
+            props.updateLocation(marker.getLatLng())
           }
         },
       }),
@@ -56,12 +59,42 @@ export default function LocationSelectorMap(props) {
     )
   }
 
+  function LeafletgeoSearch() {
+    const map = useMap();
+    useEffect(() => {
+      const provider = new OpenStreetMapProvider();
+
+      const searchControl = new GeoSearchControl({
+        provider,
+        style: 'bar',
+        showMarker: false
+      });
+
+      map.addControl(searchControl);
+      map.on('geosearch/showlocation', searchHandler)
+
+      return () => map.removeControl(searchControl);
+    }, []);
+
+    return null;
+  }
+
+  function searchHandler(result) {
+    const marker = markerRef.current
+    if (marker != null) {
+      marker.setLatLng(result.marker._latlng);
+    }
+    setPosition(result.marker._latlng);
+    props.updateLocation(result.marker._latlng);
+  }
+
   return (<div className="h-96 border">{locationSet ? <MapContainer className="h-96" center={position} zoom={17} scrollWheelZoom={true}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     <DraggableMarker />
+    <LeafletgeoSearch />
   </MapContainer>
     : <p>Väntar på kartan... Har du gett tillåtelse i webbläsaren att läsa position?</p>
   }</div>);
