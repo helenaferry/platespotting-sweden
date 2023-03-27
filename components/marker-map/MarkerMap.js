425d4d14fc79a265dc37887caa5fd03de20478fd
Helena Ferry <helena.ferry@gmail.com>
2023-03-27 21:48:23 +0200

    Typo

--- components/marker-map/MarkerMap.js
+++ components/marker-map/MarkerMap.js
@@ -93,7 +93,7 @@ export default function MarkerMap() {
                                 profile={undefined} />
                         )}
                     </AvatarGroup>
-                    {spotting.note && <p class="!m-auto max-w-[100px]">{spotting.note}</p>}
+                    {spotting.note && <p className="!m-auto max-w-[100px]">{spotting.note}</p>}
                 </div>
             </Popup>
         </Marker>)}
