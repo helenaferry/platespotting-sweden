2b0110c28ef7a2fa75bbcbc70e7184f3dd388a09
Helena Ferry <helena.ferry@gmail.com>
2023-04-25 09:57:06 +0200

    Padding on MapPlaceholder

--- components/map-placeholder/MapPlaceholder.tsx
+++ components/map-placeholder/MapPlaceholder.tsx
@@ -1,6 +1,6 @@
 export default function MapPlaceholder() {
   return (
-    <div className="flex items-center justify-center bg-[#aad3df] h-full">
+    <div className="p-6 flex items-center justify-center bg-[#aad3df] h-full">
       <p>
         Väntar på kartan... Har du gett tillåtelse i webbläsaren att läsa
         position?
